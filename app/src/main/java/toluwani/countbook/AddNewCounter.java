ac4c3b06731ec1b146dd349b81d4da5097da11dc
Toluwanimi I. Akindele <tiakindele11@gmail.com>
2017-09-27 05:09:08 -0600

    navigation to and from home

--- app/src/main/java/toluwani/countbook/AddNewCounter.java
+++ app/src/main/java/toluwani/countbook/AddNewCounter.java
@@ -1,7 +1,9 @@
 package toluwani.countbook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -12,5 +14,9 @@ public class AddNewCounter extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_new_counter);
+    }
+    public void saveNewClicked(View view) {
+            Intent i = new Intent(AddNewCounter.this, MainActivity.class);
+            startActivity(i);
     }
 }
--- app/src/main/java/toluwani/countbook/ListCountersActivity.java
+++ app/src/main/java/toluwani/countbook/ListCountersActivity.java
@@ -1,7 +1,9 @@
 package toluwani.countbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListCountersActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ListCountersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_counters);
+    }
+    public void saveNewEdit(View view) {
+        Intent i = new Intent(ListCountersActivity.this, MainActivity.class);
+        startActivity(i);
     }
 }
